52349f87f8e5ad4cef371412d356f5c89a2f572b
Jenn Schiffer <jenn@pancaketheorem.com>
2014-11-20 00:04:13 -0500

    pretty urls wowowow

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
 <?php
-  $twitterUser = strip_tags( $_GET['twitter'] );
+  // parse the url for the 'twitter' GET data
+  function parseURL() {
+    $path = array();
+    if (isset($_SERVER['REQUEST_URI'])) {
+      $request_path = explode('?', $_SERVER['REQUEST_URI']);
+
+      $path['base'] = rtrim(dirname($_SERVER['SCRIPT_NAME']), '\/');
+      $path['call_utf8'] = substr(urldecode($request_path[0]), strlen($path['base']) + 1);
+      $path['call'] = utf8_decode($path['call_utf8']);
+      if ($path['call'] == basename($_SERVER['PHP_SELF'])) {
+        $path['call'] = '';
+      }
+      $path['call_parts'] = explode('/', $path['call']);
+
+      $path['query_utf8'] = urldecode($request_path[1]);
+      $path['query'] = utf8_decode(urldecode($request_path[1]));
+      $vars = explode('&', $path['query']);
+      foreach ($vars as $var) {
+        $t = explode('=', $var);
+        $path['query_vars'][$t[0]] = $t[1];
+      }
+    }
+    return $path[call_parts][0];
+  }
+
+  $twitterUser = parseURL();
+
+  if ( !$twitterUser ) {
+	$twitterUser = strip_tags( $_GET['twitter'] );
+  }
   
   if ( $twitterUser ) {
    
